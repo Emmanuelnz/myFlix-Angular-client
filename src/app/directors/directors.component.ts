64f4609929b917a8099d8ae0f522828112dfdf0e
Emmanuelnz <emmanuelnun75@gmail.com>
2022-11-16 09:48:53 -0500

    created and styled directors component

---
+++ src/app/directors/directors.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-directors',
+  templateUrl: './directors.component.html',
+  styleUrls: ['./directors.component.scss'],
+})
+export class DirectorsComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+      Born: string;
+    }
+  ) {}
+
+  ngOnInit(): void {}
+}
